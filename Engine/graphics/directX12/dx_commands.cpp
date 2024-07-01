473e926062351c174a7bfbdd981dce28445cd882
Álvaro Cabrera <78793330+ch0pex@users.noreply.github.com>
2024-07-01 13:42:31 +0200

    dx_commnads comments

--- Engine/graphics/directX12/dx_commands.cpp
+++ Engine/graphics/directX12/dx_commands.cpp
@@ -6,9 +6,10 @@
  * @file dx_commands.cpp
  * @version 1.0
  * @date 12/03/2024
- * @brief Short description
+ * @brief Commnads Manager 
  *
- * Longer description
+ * Commnads manager methods definition 
+ * 
  */
 
 #include "dx_commands.hpp"
@@ -17,8 +18,22 @@
 
 namespace reveal3d::graphics::dx12 {
 
+/**
+ * @brief Current frame initialization
+ */
 u8 Commands::frameIndex_ = 0;
 
+/**
+ * @brief Commmands::Init
+ * 
+ * Create and Initializes the main commands and list queue 
+ * It creates 3 commands allocators one for each final render target buffer 
+ * 
+ * @param[in] device GPU device ptr
+ * 
+ * @returns
+ *  - None
+ */
 void Commands::Init(ID3D12Device *device) {
     const D3D12_COMMAND_QUEUE_DESC queueDesc {
             .Type = D3D12_COMMAND_LIST_TYPE_DIRECT,
@@ -45,11 +60,32 @@ void Commands::Init(ID3D12Device *device) {
     commandList_->Close();
 }
 
+/**
+ * @brief Commmands::Reset
+ * 
+ * Resets the current commnad allocator and 
+ * the command list 
+ * 
+ * @note Use this when you want to start recording commands
+ * 
+ * @param[in] pso Pipeline state object 
+ * 
+ * @returns
+ *  - None
+ */
 void Commands::Reset(ID3D12PipelineState *pso) {
     commandAllocators_[frameIndex_]->Reset() >> utl::DxCheck;
     commandList_->Reset(commandAllocators_[frameIndex_].Get(), pso) >> utl::DxCheck;
 }
 
+/**
+ * @brief Commmands::WaitForGPU
+ * 
+ * Waits till the GPU has released the current frame resources 
+ * 
+ * @returns
+ *  - None
+ */
 void Commands::WaitForGPU() {
     fenceValues_[frameIndex_]++;
     commandQueue_->Signal(fence_.Get(), fenceValues_[frameIndex_]) >> utl::DxCheck;
@@ -63,6 +99,15 @@ void Commands::WaitForGPU() {
     }
 }
 
+/**
+ * @brief Commmands::MoveToNextFrame
+ * 
+ * Waits till the GPU has released the current frame resources 
+ * and sets the frame index to the next 
+ * 
+ * @returns
+ *  - None
+ */
 void Commands::MoveToNextFrame() {
     const u64 currentFenceVal = fenceValues_[frameIndex_];
 
@@ -80,24 +125,47 @@ void Commands::MoveToNextFrame() {
 
     fenceValues_[frameIndex_] = currentFenceVal + 1;
 }
+
+/**
+ * @brief Commmands::Execute
+ * 
+ * Executes the recorded commands in the command list
+ * 
+ * @returns
+ *  - None
+ */
 void Commands::Execute() {
     ID3D12CommandList* ppCommandLists[] = { commandList_.Get() };
     commandQueue_->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
 }
+
+/**
+ * @brief Commmands::Execute
+ * 
+ * Executes the recorded commands in the command list
+ * 
+ * @returns
+ *  - None
+ */
 void Commands::Flush() {
     WaitForGPU();
     CloseHandle(fenceEvent_);
 }
 
+/**
+ * @brief Commmands::ResetFences
+ * 
+ * Reset all fences values to 0
+ * 
+ * @note This is only used to help out when resizing the screen 
+ * 
+ * @returns
+ *  - None
+ */
 void Commands::ResetFences() {
     frameIndex_ = 0;
     fenceValues_[1] = 0;
     fenceValues_[2] = 0;
 }
-
-void Commands::AddGraphicsList(ID3D12GraphicsCommandList *list) {
-
-}
-
 
 }
